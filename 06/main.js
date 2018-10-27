12bdc8d3a3166ec5663c9f8c851e1233156668d4
Unknown <olka27forever@gmail.com>
2018-10-27 15:25:31 +0300

    second commit

--- 06/main.js
+++ 06/main.js
@@ -1,7 +1,11 @@
+"use strict"
 var array1 = [123, "string", true, {value: 321}, null],
     array2 = ["first", "second", "last"],
     result = [];
-
-// write code here
-
+for(var i = 0; i <array1.length; i++) {
+result.push(array1[i]);
+}
+for(var i = 0; i <array2.length; i++) {
+	result.push(array2[i]);
+}
 console.log("result: ", result);
