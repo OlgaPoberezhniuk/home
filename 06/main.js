ec4b73bf332c156f3162e8c4dcf72b212de2c0c2
Unknown <olka27forever@gmail.com>
2018-10-29 21:42:58 +0300

    fix commit

--- 06/main.js
+++ 06/main.js
@@ -1,11 +1,12 @@
 "use strict"
 var array1 = [123, "string", true, {value: 321}, null],
-    array2 = ["first", "second", "last"],
-    result = [];
+   array2 = ["first", "second", "last"],
+   array2 = ["first", "second", "last"],
+   result = [];
 for(var i = 0; i <array1.length; i++) {
-result.push(array1[i]);
+result[i] = array1[i];
 }
-for(var i = 0; i <array2.length; i++) {
-	result.push(array2[i]);
+for(var i = array1.length; i <array2.length + array1.length; i++) {
+result[i] = array2[i - array1.length];
 }
 console.log("result: ", result);
