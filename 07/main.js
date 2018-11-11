1314e65cf0102306a0bca1e3114592636c5ce96a
Unknown <olka27forever@gmail.com>
2018-11-11 18:28:44 +0300

    second commit

--- 07/main.js
+++ 07/main.js
@@ -7,5 +7,47 @@ result = prioritySort(data, ['number', 'null', 'string', 'object', 'undefined', 
 console.log('result', result);
 
 function prioritySort(array, dataPriority) {
-    // write code here
+
+function compareNumeric(a, b) {
+        if (a>b) return 1;
+        if (a<b) return -1;
+    };
+var number = [1, 500, 30];
+number.sort(compareNumeric);
+//console.log(number);
+    
+    //еще вариант
+//var number = [30, 1, 500];
+    //number.sort(function (a, b) {
+    //return a - b;
+    //});
+//console.log(number); 
+
+function compareString(a,b) {
+        if (a>b) return 1;
+        if (a<b) return -1;
+    };
+var string = ['firstString', 'abc'];
+string.sort(compareString);
+//console.log (string);
+
+//еще вариант
+//var string = ['firstString', 'abc'];
+//string.sort();
+//console.log(string);
+
+var boolean = [true, false, true];
+boolean.sort(function (a, b) {
+return b - a;
+});
+//console.log (boolean);
+
+var array = [null];
+
+var object  =  {test: 'Object'};
+
+var undefined = [undefined];
+var result = number.concat(null, string, object, undefined, boolean);
+console.log('result', result);
 }
+
