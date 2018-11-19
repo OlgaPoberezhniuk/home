7abe48076fba2cc90a9fe3b86e4c746750faf3ff
Unknown <olka27forever@gmail.com>
2018-11-19 22:35:20 +0300

    second commit

--- 11/slide.js
+++ 11/slide.js
@@ -1,14 +1,35 @@
 document.querySelectorAll('.slides').forEach(function (element) {
     var slideSize = element.clientWidth;
-        currentPosition = 0;
+    currentPosition = 0;
 
     setInterval(function () {
-        currentPosition += slideSize;
+        currentPosition = element.scrollLeft + slideSize;
 
         if (currentPosition >= element.scrollWidth) {
             currentPosition = 0;
         }
 
         element.scroll({left: currentPosition, behavior: 'smooth'});
-    }, 2000);
+    }, 6000);
 });
+
+var slider = document.getElementsByClassName('slides')[0];
+document.getElementById('slider-left').onclick = sliderRight;
+
+function sliderLeft() {
+    var offset = slider.scrollLeft + 800;
+    if (offset >= slider.scrollWidth) {
+        offset = 0;
+    }
+    slider.scroll({left: offset, behavior: 'smooth'});
+}
+
+document.getElementById('slider-right').onclick = sliderLeft;
+
+function sliderRight() {
+    var offset = slider.scrollLeft - 800;
+    if (offset <= 0) {
+        offset = slider.scrollWidth;
+    }
+    slider.scroll({left: offset, behavior: 'smooth'});
+}
