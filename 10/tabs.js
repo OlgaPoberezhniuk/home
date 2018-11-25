96f83f9707272b3f0ae8c5db1ac64c3a57105bfb
Unknown <olka27forever@gmail.com>
2018-11-25 15:45:40 +0300

    second commit

--- 10/tabs.js
+++ 10/tabs.js
@@ -1,19 +1,42 @@
 (function () {
+
     "use strict";
 
+
     document.querySelectorAll('.tabs').forEach(function (tabsElement) {
-        var selectElement = tabsElement.querySelector('select'),
-            tabsContainerElement = tabsElement.querySelector('.tabs-tabsContainer');
+
+        var selectElement = tabsElement.querySelector('.tabs-select'),
+
+            tabsContainerElement = tabsElement.querySelector('.tabs-tabsContainer'),
+
+            newTab = "tabs-container-tab-selected";
+
 
         console.log('current value: ', selectElement.value);
 
+        getTabByName(tabsContainerElement, selectElement.value).classList.add(newTab);
+
+
         selectElement.onchange = function () {
+
             console.log('new value: ', selectElement.value);
 
+
+            document.querySelector("." + newTab).classList.remove(newTab);
+
+            getTabByName(tabsContainerElement, selectElement.value).classList.add(newTab);
+
         };
+
     });
 
+
     function getTabByName(tabsContainerElement, name) {
+
         return tabsContainerElement.querySelector('[data-name=' + name + ']');
+
     }
+
 })();
+
+
